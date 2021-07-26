b9b2483f8119ef06353f854a486e4c8ec1cdfcaa
Antonio <antonio.sanna2706@gmail.com>
2021-07-26 17:21:13 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -14,4 +14,29 @@ console.log(bombe);
 var listNumberPlayers = [];
 var numberAllowed = 0;
 
+for ( var i = 0; i < 6; i++) {
+    var numberPlayer = parseInt(
+        prompt("Inserisci un numero compreso da 1 a 100")
+    );
 
+    if(bombe.includes(numberPlayer)) {
+        console.log('Sei Esploso!');
+        break;
+    } else if (numberPlayer < 1 || numberPlayer > 100) {
+        console.log("hai inserito un numero vietato, hai perso!");
+        break
+    }else if (isNaN(numberPlayer)) {
+        console.log('Non hai inserito un numero, hai perso!');
+        break
+    }  else {
+        listNumberPlayers.push(numberPlayer);
+        console.log("Hai inserito:" + numberPlayer);
+        console.log("Ottimo,non sei esploso");
+        numberAllowed++;
+    }
+}
+console.log("hai fatto un punteggio di " + numberAllowed);
+
+function generaNumero(min,max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
